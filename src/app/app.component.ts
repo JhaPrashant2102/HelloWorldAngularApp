59131bd4ca951ba604f7e61d71cd2ce838384df5
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-11-28 21:37:06 +0530

    demonstrated two way data binding using validation for user name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   title = 'Hello from this.title(data binding)';
   imgUrl = "";
   srcUrl = "";
+  userName: string="";
+  nameError: string="";
 
   ngOnInit():void{
     this.title="hello from google";
@@ -20,5 +22,15 @@ export class AppComponent {
     console.log("Save button is clicked",$event);
     window.open(this.srcUrl,"_blank");
     // blank->opens url in new tab
+  }
+
+  onInput($event){
+    console.log("Change event occurred!");
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is incorrect";
   }
 }
